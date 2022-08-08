17803e84e6234d4388f07b33cde5138e1e7cf4d2
张艳华 <939593210@qq.com>
2022-08-08 14:02:59 +0800

    feat:关于架子的继续更新

---
+++ src/views/Dashboard/index.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: ZYH
+ * @Date: 2022-08-08 11:44:53
+ * @LastEditTime: 2022-08-08 11:44:55
+ * @Description: 
+-->
+<template>
+    <div>
+        首页
+    </div>
+</template>
+<script lang='ts' setup>
+</script>
+<style lang="scss" scoped>
+
+</style>
+
---
+++ src/views/ErrorPage/404.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: ZYH
+ * @Date: 2022-08-08 11:26:19
+ * @LastEditTime: 2022-08-08 11:48:13
+ * @Description: 
+-->
+<template>
+  <Result
+    status="404"
+    title="404"
+    sub-title="Sorry, the page you visited does not exist."
+  >
+    <template #extra>
+      <a-button type="primary" @click="handelBack">返回首页</a-button>
+    </template>
+  </Result>
+</template>
+<script lang="ts" setup>
+import { Result } from "ant-design-vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const handelBack = () => {
+  router.push({ name: "dashboard" });
+};
+</script>
+<style lang="scss" scoped></style>
